d01a7b451655a958a46452f415c8c5220c75048a
Milansuman <milansuman2016@gmail.com>
2023-03-05 19:24:49 +0530

    Add templating engine

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ Patient Portal is a PWA that makes it easy for clinics to set up appointments wi
 ## TODO
 
 - [x] Set up project skeleton
+- [x] Add templating engine so clinics can customize the main view
 - [ ] Set up icons for the PWA
 - [ ] Implement form
 - [ ] Test and improve security and compliance with NIST standards
 - [ ] Add configuration options for clinics
+- [ ] Add localisation
--- server.js
+++ server.js
@@ -1,14 +1,41 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
+const fs = require("fs");
 
+//Predefined constants
 const PORT = 8080;
+const TEMPLATE_EXT = "view"; //file extension of templates
+
 const app = express();
 
 //Setting up static resources
 app.use(express.static(path.join(__dirname, "static/")));
 
+//Setting up a templating engine so clinics can easily change text to suit their needs
+app.engine(TEMPLATE_EXT, (filePath, options, callback) => {
+    fs.readFile(filePath, (err, content) => {
+        
+        if(err) return callback(err);
+
+        const rendered = content.toString()
+            .replace("$description$", `${options.description}`)
+            .replace("$clinic-name$", `${options.clinicName}`)
+            .replace("$url$", `${options.url}`)
+            .replace("$address$", `${options.address}`)
+            .replace("$email$", `${options.email}`);
+
+        return callback(null, rendered);
+    });
+});
+
+app.set("views", path.join(__dirname, "templates/"));
+app.set("view engine", TEMPLATE_EXT);
+
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    fs.readFile(path.join(__dirname, "config.json") , (err, content) => {
+        if (err) throw err;
+        res.render("index", JSON.parse(content.toString()));
+    })
 });
 
 app.listen(PORT);
